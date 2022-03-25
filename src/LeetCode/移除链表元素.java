2a8f5f9445637b85b5642557dfc206c5479036f8
阿丁 <dinqidong@gmail.com>
2022-03-25 21:09:38 +0800

    数据结构-链表-移除链表元素-使用虚拟头节点

--- src/LeetCode/移除链表元素.java
+++ src/LeetCode/移除链表元素.java
@@ -65,4 +65,27 @@ class ListNode {
       ListNode() {}
       ListNode(int val) { this.val = val; }
       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public ListNode(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("参数有误");
+        }
+        this.val = arr[0];
+        ListNode cur = this;
+        for (int i = 1; i < arr.length; i++) {
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        ListNode cur = this;
+        while (cur != null) {
+            res.append(cur.val + "->");
+            cur = cur.next;
+        }
+        return res.toString();
+    }
  }
